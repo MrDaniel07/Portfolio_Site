2fdcfb6e4369610e31d7282ff957e841d0409bf3
Anyahuru Daniel <anyahurudaniel55@gmail.com>
2026-02-04 12:47:33 +0100

    Update service worker version and resource hashes for web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fcb3d40700de61626a629922a12c4dc1",
+const RESOURCES = {"flutter_bootstrap.js": "1843d71e366ec6983c0a54884ccbd0c0",
 "version.json": "b67f87bd2e4476c323de8773a6f36981",
-"index.html": "b8cb1495ed6ba4df51373c72c2ba1671",
-"/": "b8cb1495ed6ba4df51373c72c2ba1671",
+"index.html": "e9218aec81094e64de45d110d73dce86",
+"/": "e9218aec81094e64de45d110d73dce86",
 "main.dart.js": "2a873c48bf272715fb888dbe4ee4edf6",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "_redirects": "5b8279a29d8a80113d6d7c2989fbf147",
